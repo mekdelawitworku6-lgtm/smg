f27519387903c1a237739db8461069cc45562db7
lawit <mekdelawitworku6@gmail.com>
2026-06-03 08:22:41 -0700

    feat: implement offline mode, staff management, and dashboard improvements

--- salon-frontend/dev-dist/sw.js
+++ salon-frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.mg80gt0h3do"
+    "revision": "0.j8gp87tjbts"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
